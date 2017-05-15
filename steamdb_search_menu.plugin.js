362fdca3ae044ec974766ddad366e55c2c1447e8
Dmitry Romanenko <Dmitry@Romanenko.in>
2017-05-15 02:27:34 -0400

    Shorter title for context menu

--- steamdb_search_menu.plugin.js
+++ steamdb_search_menu.plugin.js
@@ -15,7 +15,7 @@ searchSteamDBAppOption.prototype.start = function(){
               setTimeout(function(){
                   if($('.context-menu .item span')[1].innerHTML.indexOf('Google') > 0)
                     $('.context-menu .item-group:contains("Google")')
-                        .append('',"<div class=\"item-group\" onclick=\"window.open('https://steamdb.info/search/?a=app&q='+encodeURIComponent(window.getSelection().toString()))\"><div class=\"item\"><span>Search AppName on SteamDB</span> </div></div></div ><div class=\"hint\">");
+                        .append('',"<div class=\"item-group\" onclick=\"window.open('https://steamdb.info/search/?a=app&q='+encodeURIComponent(window.getSelection().toString()))\"><div class=\"item\"><span>Search on SteamDB</span> </div></div></div ><div class=\"hint\">");
               },250);
               // if you are reading this and have a better solution to this, feel free to leave a PR or pm me about it -cate#4444/Ckath
           }
